f12d024fda7eda17ce85a82b97e684dd56c19d84
Sai Deepthi Cheerladinne <saideepthicheerladinne@gmail.com>
2025-07-24 21:33:11 +0530

    first commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,23 @@
+import type { Route } from "./+types/home";
+
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
+  ];
+}
+
+export default function Home() {
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2> Review your submissions and check AI-powered feedback.</h2>
+
+      </div>
+    </section>
+
+  </main>
+}
+
