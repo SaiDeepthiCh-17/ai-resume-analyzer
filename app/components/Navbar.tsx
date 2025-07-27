e22d7307e3a39d9854c9cc1fed43fdd3cd722635
Sai Deepthi Cheerladinne <saideepthicheerladinne@gmail.com>
2025-07-27 16:28:30 +0530

    Authentication

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router";
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/">
+        <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+      </Link>
+      <Link to='/upload' className="primary-button w-fit">
+        Upload Resume
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,12 @@
+import { resumes } from "../constants";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+// import usePuterStore from wherever it is defined
+// import { usePuterStore } from "path/to/store"; // <-- update this path!
+
 import type { Route } from "./+types/home";
 
 
@@ -9,14 +18,34 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const { auth } = usePuterStore();
+    
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      if (!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+    
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2> Review your submissions and check AI-powered feedback.</h2>
 
       </div>
+    
+
+      {resumes?.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+         ))}
+       </div>
+    )}
     </section>
+
 
   </main>
 }
