937534692d9dae72f336b77d23ce23680ae5a38a
Sai Deepthi Cheerladinne <saideepthicheerladinne@gmail.com>
2025-08-16 20:09:00 +0530

    Cloud torage, PDF-Image conversion, Key-Value storage and AI feedback

--- app/lib/puter.ts
+++ app/lib/puter.ts
@@ -115,6 +115,7 @@ export const usePuterStore = create<PuterStore>((set, get) => {
       },
     });
   };
+  
 
   const checkAuthStatus = async (): Promise<boolean> => {
     const puter = getPuter();
@@ -350,7 +351,7 @@ export const usePuterStore = create<PuterStore>((set, get) => {
           ],
         },
       ],
-      { model: "claude-sonnet-4" }
+      { model: "claude-3-7-sonnet" }
     ) as Promise<AIResponse | undefined>;
   };
 
